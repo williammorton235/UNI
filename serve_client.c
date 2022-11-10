42940404ef3838aa32c49e73644a1d83c1a8d42f
williammorton235 <71633125+williammorton235@users.noreply.github.com>
2022-11-10 18:06:14 +0000

    Update serve_client.c

--- serve_client.c
+++ serve_client.c
@@ -6,40 +6,48 @@
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 
 void *downtime() {
-    for (int i; i<3; i++){
+    for (int i = 0; i<3; i++){
         pthread_mutex_lock(&mutex1);
+        printf("down");
         Node *temp = root;
         root = balanceTree(root);
         freeSubtree(temp);
         pthread_mutex_unlock(&mutex1);
         sleep(2);
     }
+    return NULL;
 }
 
 void *ServeClient(char *client) {
     FILE *fp;
     fp = fopen(client, "r");
     char func[100];
-    int data[100];
-    while (fscanf(fp, "%s %d", func, data) == 1){
-        if (func == "addNode"){
+    int data;
+    while (fscanf(fp, "%s %d", func, &data) != EOF){
+        if (!strcmp(func, "addNode")){
             pthread_mutex_lock(&mutex1);
+            printf("[%s]insertNode <%d>\n", client, data);
             root = addNode(root, data);
             pthread_mutex_unlock(&mutex1);
-            printf("[%s]insertNode <%d>\n", client, data);
         }
-        else if (func == "removeNode"){
+        else if (!strcmp(func, "removeNode")){
             pthread_mutex_lock(&mutex1);
+            printf("[%s]deleteNodeNode <%d>\n", client, data);
             root = removeNode(root, data);
             pthread_mutex_unlock(&mutex1);
-            printf("[%s]deleteNodeNode <%d>\n", client, data);
         }
-        else if (func == "countNodes"){
+        else if (!strcmp(func, "countNodes")){
+            printf("1");
+            pthread_mutex_lock(&mutex1);
             printf("[%s]countNodes = <%d>\n", client, countNodes(root));
+            pthread_mutex_unlock(&mutex1);
         }
-        else if (func == "avgSubtree"){
-            printf("“[%s]avgSubtree = <%f>\n", client, avgSubtree(root));
+        else if (!strcmp(func, "avgSubtree")){
+            printf("2");
+            pthread_mutex_lock(&mutex1);
+            printf("[%s]avgSubtree = <%f>\n", client, avgSubtree(root));
+            pthread_mutex_unlock(&mutex1);
         }
     }
-
+    return NULL;
 }
